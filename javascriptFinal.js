7c34bcd18ffa47981dfa37915ce684f860094f4a
User1928374 <andrewochockey@gmail.com>
2025-04-07 00:15:29 -0400

    final update to project

--- javascriptFinal.js
+++ javascriptFinal.js
@@ -10,11 +10,14 @@ function loadPage(page){
                 document.getElementById('page-section').innerHTML = data;
 
                 const searchBar = document.getElementById('searchbar');
+                const searchResult = document.getElementById('search-result');
 
                 if(page != 'index'){
                     searchBar.style.visibility = "hidden";
+                    searchResult.style.display = "none";
                 }else{
                     searchBar.style.visibility = "visible";
+                    searchResult.style.visibility = "visible";
                 }
         })
         .catch(error => console.error('Error loading page: ', error))
@@ -38,6 +41,7 @@ searchButton.addEventListener('click', function() {
 
         let randomNumberThree = Math.floor(Math.random() * 3);
         let randomNumberTwo = Math.floor(Math.random() * 2);
+        let nextCountryIndex = (randomNumberThree + 1) % 3;
 
         if(searchInputLower == 'beach' || searchInputLower == 'beaches'){
             searchInputLower = 'beaches';
@@ -59,7 +63,6 @@ searchButton.addEventListener('click', function() {
                 }
             }
 
-            let nextCountryIndex = (randomNumberThree + 1) % 3;
             const countryTwo = json.countries[nextCountryIndex];
             if (countryTwo) {
                 const foundCityTwo = countryTwo.cities[randomNumberTwo];
@@ -69,6 +72,33 @@ searchButton.addEventListener('click', function() {
                     resultTwo.querySelector('p').textContent = foundCityTwo.description;
                 }
             }
+        }else if(searchInputLower == 'temples'){
+            const templeOne = json.temples[randomNumberTwo];
+            const nextTempleIndex = (randomNumberTwo + 1) % 2;
+
+            const templeTwo = json.temples[nextTempleIndex];
+            
+            resultOne.querySelector('img').src = templeOne.imageUrl;
+            resultOne.querySelector('h4').textContent = templeOne.name;
+            resultOne.querySelector('p').textContent = templeOne.description;
+
+            resultTwo.querySelector('img').src = templeTwo.imageUrl;
+            resultTwo.querySelector('h4').textContent = templeTwo.name;
+            resultTwo.querySelector('p').textContent = templeTwo.description;
+
+        }else if(searchInputLower == 'beaches'){
+            const beachOne = json.beaches[randomNumberTwo];
+            const nextBeachIndex = (randomNumberTwo + 1) % 2;
+
+            const beachTwo = json.beaches[nextBeachIndex];
+            
+            resultOne.querySelector('img').src = beachOne.imageUrl;
+            resultOne.querySelector('h4').textContent = beachOne.name;
+            resultOne.querySelector('p').textContent = beachOne.description;
+
+            resultTwo.querySelector('img').src = beachTwo.imageUrl;
+            resultTwo.querySelector('h4').textContent = beachTwo.name;
+            resultTwo.querySelector('p').textContent = beachTwo.description;
         }
 
         searchResult.style.visibility = 'visible';
@@ -76,5 +106,23 @@ searchButton.addEventListener('click', function() {
     })
     .catch(error => console.error('Error fetching the JSON:', error));
 
+});
+
+const resetButton = document.getElementById('reset-button');
+
+resetButton.addEventListener('click', function(){
+    const searchResult = document.getElementById('search-result');
+
+    const resultOne = document.getElementById('result-1');
+    const resultTwo = document.getElementById('result-2');
+    
+    resultOne.querySelector('img').src = '';
+    resultOne.querySelector('h4').textContent = '';
+    resultOne.querySelector('p').textContent = '';
+    resultTwo.querySelector('img').src = '';
+    resultTwo.querySelector('h4').textContent = '';
+    resultTwo.querySelector('p').textContent = '';
+
+    searchResult.style.display = 'none';
 });
 
