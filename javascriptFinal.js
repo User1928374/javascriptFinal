eeee8b9dfa5b28ed399dccaf3b28563403e1a4ad
User1928374 <andrewochockey@gmail.com>
2025-04-07 00:14:54 -0400

    finalized project

--- javascriptFinal.js
+++ javascriptFinal.js
@@ -19,4 +19,62 @@ function loadPage(page){
         })
         .catch(error => console.error('Error loading page: ', error))
 }
-/*document.addEventListener('DOMContentLoaded' , () => loadPage('home'));*/
+
+
+const searchButton = document.getElementById('search-button');
+
+searchButton.addEventListener('click', function() {
+
+    fetch('../javascriptFinal_api.json')
+    .then(response => response.json())
+    .then(json => {
+
+        const searchResult = document.getElementById('search-result');
+        const searchInput = document.getElementById('search-input').value;
+        let searchInputLower = searchInput.toLowerCase();
+
+        const resultOne = document.getElementById('result-1');
+        const resultTwo = document.getElementById('result-2');
+
+        let randomNumberThree = Math.floor(Math.random() * 3);
+        let randomNumberTwo = Math.floor(Math.random() * 2);
+
+        if(searchInputLower == 'beach' || searchInputLower == 'beaches'){
+            searchInputLower = 'beaches';
+        } else if(searchInputLower == 'temple' || searchInputLower == 'temples'){
+            searchInputLower = 'temples';
+        } else if(searchInputLower == 'country' || searchInputLower == 'countries'){
+            searchInputLower = 'countries';
+        }
+
+        if(searchInputLower == 'countries'){
+            
+            const countryOne = json.countries[randomNumberThree];
+            if (countryOne) {
+                const foundCityOne = countryOne.cities[randomNumberTwo];
+                if (foundCityOne) {
+                    resultOne.querySelector('img').src = foundCityOne.imageUrl;
+                    resultOne.querySelector('h4').textContent = foundCityOne.name;
+                    resultOne.querySelector('p').textContent = foundCityOne.description;
+                }
+            }
+
+            let nextCountryIndex = (randomNumberThree + 1) % 3;
+            const countryTwo = json.countries[nextCountryIndex];
+            if (countryTwo) {
+                const foundCityTwo = countryTwo.cities[randomNumberTwo];
+                if (foundCityTwo) {
+                    resultTwo.querySelector('img').src = foundCityTwo.imageUrl;
+                    resultTwo.querySelector('h4').textContent = foundCityTwo.name;
+                    resultTwo.querySelector('p').textContent = foundCityTwo.description;
+                }
+            }
+        }
+
+        searchResult.style.visibility = 'visible';
+
+    })
+    .catch(error => console.error('Error fetching the JSON:', error));
+
+});
+
